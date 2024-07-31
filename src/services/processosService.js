a2486476c26474fdb1221d0b15f5aba0cb74b826
Murilo Fernandes <murilofsilva11@gmail.com>
2024-07-31 00:14:49 -0400

    Revert "feat[LEDES-0001]: Detalhes do processo"
    This reverts commit 9d1b384c095fb5b773fc46604720c2884b1b4dd1.

--- src/services/processosService.js
+++ src/services/processosService.js
@@ -28,38 +28,4 @@ const getProcessoById = async (id) => {
   }
 };
 
-const validarDataPrescricao = async (id) => {
-  try {
-    const response = await fetch(`${API_URL}/${id}`, {
-      method: 'PUT'
-    });
-    if (!response.ok) {
-      throw new Error('Erro ao validar data de prescrição');
-    }
-    return true;
-  } catch (error) {
-    console.error('Erro ao validar data de prescrição:', error);
-    return false;
-  }
-};
-
-const atualizarConteudoDocumento = async (id, conteudo) => {
-  try {
-    const response = await fetch(`${API_URL}/${id}/documento`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(conteudo),
-    });
-    if (!response.ok) {
-      throw new Error('Erro ao atualizar conteúdo do documento');
-    }
-    return true;
-  } catch (error) {
-    console.error('Erro ao atualizar conteúdo do documento:', error);
-    return false;
-  }
-};
-
-export default { getProcessos, getProcessoById, validarDataPrescricao, atualizarConteudoDocumento };
+export default { getProcessos, getProcessoById };
